429f7ff931717c4a99b9d522d75ce45147a26bbb
Ssemugabi Martin <76908155+dombreaker36@users.noreply.github.com>
2022-07-25 19:30:40 +0300

    fix failing server

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import  app from './app'
+import app from './app.js'
+import { errors } from 'celebrate';
 
 const PORT = process.env.PORT || 5000
 
-app.use('/questions', questionRoutes)
+app.use(errors());
 
 app.listen(PORT, console.log(`server running on Port ${PORT}`))
 

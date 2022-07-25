f567db7e82a4a601173325df21a7df5708758fd3
Ssemugabi Martin <76908155+dombreaker36@users.noreply.github.com>
2022-07-25 20:24:58 +0300

    restructured project files

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,26 +1,26 @@
-import request from 'supertest'
-import app from '../app'
+import request from "supertest";
+import app from "../../app";
 
-describe("server route", ()=>{
-  it('return the home page with a message', async ()=>{
-    const res = await request(app).get('/');
-    expect(res.status).toBe(200)
-  })
-})
-
-describe('question routes', ()=>{
-  it('get all questions', async ()=>{
-    const res =  await request(app).get('/questions');
+describe("server route", () => {
+  it("return the home page with a message", async () => {
+    const res = await request(app).get("/");
     expect(res.status).toBe(200);
-    expect(res.body[0].question).toequal('Questions')
-  })
+  });
+});
 
-  it('if the question doesnt exist', async ()=>{
-    const res = await request(app).get('/questions/p');
-    expect(res.status).toBe(400)
+describe("question routes", () => {
+  it("get all questions", async () => {
+    const res = await request(app).get("/questions");
+    expect(res.status).toBe(200);
+    expect(res.body[0].question).toequal("Questions");
+  });
 
-    expect(res.body.message).toequal('Question not found')
-  })
+  it("if the question doesnt exist", async () => {
+    const res = await request(app).get("/questions/p");
+    expect(res.status).toBe(400);
+
+    expect(res.body.message).toequal("Question not found");
+  });
 
   // it('Post a question', async ()=>{
   //   const res = await request(app).post('/question')
@@ -29,5 +29,4 @@ describe('question routes', ()=>{
   // })
 
   // it('Update a specific Question', async ())
-})
-
+});
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import app from './app.js'
-import { errors } from 'celebrate';
+import express from "express";
+import { errors } from "celebrate";
+import bodyParser from "body-parser";
+import questions from "./routes/questions.js";
 
-const PORT = process.env.PORT || 5000
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/questions", questions);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Our Home Page");
+});
 
 app.use(errors());
 
-app.listen(PORT, console.log(`server running on Port ${PORT}`))
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, console.log(`server running on Port ${PORT}`));
 
+export default app;
